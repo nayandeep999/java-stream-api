0af0b72e33b68b7ee1a7560f628fe77f24d00883
Nayandeep <nayandeepsen@gmail.com>
2026-02-04 09:58:52 +0530

    added class to sort list of strings in alphabetical order

---
+++ src/com/kodewala/streams/importantques/SortListOfStringByAlphabets.java
@@ -0,0 +1,17 @@
+package com.kodewala.streams.importantques;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortListOfStringByAlphabets {
+
+	public static void main(String[] args) {
+		List<String> randomNames = Arrays.asList("Nayandeep", "Satrugan", "Sambu", "Ram", "Baharat", "Lakshman");
+		List<String> sortedListByAlphabetsRev = randomNames.stream().sorted(Comparator.reverseOrder()).toList();
+		System.out.println("Reverse order: " + sortedListByAlphabetsRev);
+		List<String> sortedListByAlphabets = randomNames.stream().sorted().toList();
+		System.out.println("Normal ascending order: " + sortedListByAlphabets);
+	}
+
+}
