0c144437930c13ce624f15bad7c6ef74f920a910
Nayandeep <nayandeepsen@gmail.com>
2026-02-05 12:00:00 +0530

    added another way to sort the list of alphabets

--- src/com/kodewala/streams/importantques/SortListOfStringByAlphabets.java
+++ src/com/kodewala/streams/importantques/SortListOfStringByAlphabets.java
@@ -8,7 +8,8 @@ public class SortListOfStringByAlphabets {
 
 	public static void main(String[] args) {
 		List<String> randomNames = Arrays.asList("Nayandeep", "Satrugan", "Sambu", "Ram", "Baharat", "Lakshman");
-		List<String> sortedListByAlphabetsRev = randomNames.stream().sorted(Comparator.reverseOrder()).toList();
+		List<String> sortedListByAlphabetsRev = randomNames.stream()
+				.sorted(Comparator.reverseOrder()/* (a,b) -> b.compareTo(a) */).toList();
 		System.out.println("Reverse order: " + sortedListByAlphabetsRev);
 		List<String> sortedListByAlphabets = randomNames.stream().sorted().toList();
 		System.out.println("Normal ascending order: " + sortedListByAlphabets);
