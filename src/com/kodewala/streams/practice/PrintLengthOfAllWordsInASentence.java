14fe9bd031c5ac933b9bed407aa873ee41b18280
Nayandeep <nayandeepsen@gmail.com>
2026-02-08 11:34:58 +0530

    added practice class to print length of all words in a given sentence

---
+++ src/com/kodewala/streams/practice/PrintLengthOfAllWordsInASentence.java
@@ -0,0 +1,16 @@
+package com.kodewala.streams.practice;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PrintLengthOfAllWordsInASentence {
+
+	public static void main(String[] args) {
+		String str = "Hello my name is Seema Sen Gupta";
+		Map<String, Integer> map = Arrays.stream(str.split(" "))
+				.collect(Collectors.toMap(word -> word, word -> word.length()));
+		map.forEach((k, v) -> System.out.println("Length of " + k + " is: " + v));
+	}
+
+}
